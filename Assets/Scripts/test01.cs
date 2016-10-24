78c0d78f8834f146c13a4e12d409bb886cfbbc4c
MoonRaito <394950240@qq.com>
2016-10-24 18:42:12 +0800

    create grid

--- Assets/Scripts/test01.cs
+++ Assets/Scripts/test01.cs
@@ -1,10 +1,15 @@
 ﻿using UnityEngine;
 using System.Collections;
 
+
+//using System.Linq;
+
+using System.Collections.Generic;//C# 包中的类
+
 public class test01 : MonoBehaviour {
 
-	private float width = 40;
-	private float height = 40;
+	private int width = 40;
+	private int height = 40;
 
 	// Use this for initialization
 	void Start () {
@@ -38,11 +43,33 @@ public class test01 : MonoBehaviour {
 //		cube.transform.localScale = new Vector3(1.0F, 1.0F, 1.0F);
 //		cube.GetComponent<MeshRenderer>().material.color = new Color(1f, 0.92f, 0.016f, 1f);
 
-		Debug.Log ("1");
-		Maze maze = new Maze ();
-		Debug.Log ("2");
+		Debug.Log ("Maze start");
+		Maze maze = new Maze (width,height);
+		maze.perfect = true;
+		maze.braid = false;
 
+		maze.init ();
 
+		maze.startNode = maze.getRandomNode ();
+		do {
+			maze.endNode = maze.getRandomNode ();
+		} while(maze.startNode == maze.endNode);
+			
+		maze.generate();
+
+		List<List<Node>> grid = maze.grid;
+
+		List<Node> ln = null;
+		Node n = null;
+		for(int i = 0;i<grid.Count;i++){
+			ln = grid [i];
+			for(int j = 0;j<ln.Count;j++){
+				n = ln [j];
+				initGrid(n);
+			}
+		}
+
+		Debug.Log ("Maze end");
 
 	}
 	
@@ -50,13 +77,146 @@ public class test01 : MonoBehaviour {
 	void Update () {
 		
 	}
+
+
+	private void initGrid(Node node){
+		GameObject cube = GameObject.CreatePrimitive(PrimitiveType.Cube);
+		cube.transform.localPosition = new Vector3 (9.0f*node.x,0.5F,9.0f*node.y);
+		cube.transform.localScale = new Vector3(9.0F, 1.0F, 9.0F);  // w z h
+		cube.GetComponent<MeshRenderer>().material.color = new Color(1f*node.x/10, 0.92f*node.y/10, 0.016f, 1f);
+
+		bool left = (node.value & Maze.Direction.W) != Maze.Direction.W;
+		bool top = (node.value & Maze.Direction.N) != Maze.Direction.N;
+
+		if(left&&top){
+			GameObject cube1 = GameObject.CreatePrimitive(PrimitiveType.Cube);
+			cube1.transform.localPosition = new Vector3 ((9.0f*node.x),1.5F,9.0f*node.y);
+			cube1.transform.localScale = new Vector3(1.0F, 1.0F, 9.0F);  // w z h
+			cube1.GetComponent<MeshRenderer>().material.color = new Color(1f, 0.92f, 0.016f*node.y/10, 1f*node.x/10);
+
+
+			GameObject cube2 = GameObject.CreatePrimitive(PrimitiveType.Cube);
+			cube2.transform.localPosition = new Vector3 ((9.0f*node.x),1.5F,9.0f*node.y);
+			cube2.transform.localScale = new Vector3(9.0F, 1.0F, 1.0F);  // w z h
+			cube2.GetComponent<MeshRenderer>().material.color = new Color(1f, 0.92f, 0.016f*node.y/10, 1f*node.x/10);
+		}
+
+	}
 }
 
 public class Maze : MazeBase{
-	public int width = 20;
-	public int height = 20;
+//	new public int width = 20;
+//	new public int height = 20;
 
 	public bool perfect= true;
 	public bool braid = false;
 	public bool checkOver = false;
+
+	public bool foundEndNode;
+
+	// 默认构造
+	public Maze(){
+
+	}
+
+	// 初始化
+	public Maze(int width,int height){
+		this.width = width;
+		this.height = height;
+	}
+
+	// 这里就发生了重写,也可以说是隐藏了父类的方法. 这样做了之后就不能再使用父类的同名方法了.
+	// 获取邻居
+	public override List<object> getNeighbor(){
+		List<List<object>> list = (List<List<object>>)this.neighbors["list"];
+
+		System.Random random = new System.Random();
+		int key = random.Next(0,list.Count);
+
+		List<object> n = list[key];
+		return n;
+	}
+		
+
+	// 验证 附近的节点是否有效
+	public override bool isValid(Node nearNode,Node node,int dir) {
+		if (nearNode == null  || nearNode.value != 0) {
+			return false;
+		}
+		if (nearNode.value == 0) {
+			return true;
+		}
+		if (this.perfect || this.braid) {
+			return false;
+		}
+//		int c = nearNode.x,
+//		r = nearNode.y;
+		// 用于生成一种非Perfect迷宫
+//		this.checkCount[c + "-" + r] = this.checkCount[c + "-" + r] || 0;
+//		var count = ++this.checkCount[c + "-" + r];
+//		return Math.random() < 0.3 && count < 3;
+		return false;
+	}
+
+	public override void beforeBacktrace(){
+		if (!this.braid) {
+			return;
+		}
+
+	} 
+
+	public override void updateCurrent(){
+		if (this.braid) {
+			return;
+		}
+		System.Random random = new System.Random();
+		// 每步有 10% 的概率 进行回溯
+		if (random.Next(1,10) == 1) {
+			this.backtrace();
+		}
+	}
+
+	public override int getTraceIndex(){
+		int len = this.trace.Count;
+
+		if (this.braid) {
+			return len - 1;
+		}
+
+		// 按一定的概率随机选择回溯策略
+		System.Random random = new System.Random();
+		int r = random.Next (0, 9)/10;
+		var idx = 0;
+		if (r < 0.5) {
+			idx = len - 1;
+		} else if (r < 0.7) {
+			idx = len >> 1;
+		} else if (r < 0.8) {
+			idx = len * random.Next (0, 9)/10 >> 0;
+		}
+		return idx;
+	}
+
+	public override void afterGenrate(){
+		if (this.braid && this.getRoadCount(this.startNode)<2) {
+			this.currentDirCount = 1000;
+			this.setCurrent(this.startNode);
+			this.nextStep();
+		}
+	}
+
+	public override bool isOver(){
+		if (!this.checkOver) {
+			return false;
+		}
+		if (this.currentNode == this.endNode) {
+			this.foundEndNode = true;
+		}
+		// 当探索到迷宫终点, 且探索了至少一半的区域时,终止迷宫的生成
+		if (this.foundEndNode && this.stepCount >= this.size / 2) {
+			return true;
+		}
+		return false;
+	}
+
 }
--- Assets/Scripts/tool/MazeBase.cs
+++ Assets/Scripts/tool/MazeBase.cs
@@ -1,27 +1,30 @@
 ﻿using UnityEngine;
 using System.Collections;
 
+using System.Linq;
+
 using System.Collections.Generic;//C# 包中的类
 
 public class MazeBase  : MonoBehaviour {
 
-	private int width;
-	private int height;
+	public int width;
+	public int height;
 
-	List<List<Node>> grid = new List<List<Node>>();  
+	public List<List<Node>> grid = new List<List<Node>>();  
 
 	private int currentDir;      	// 当前的方向
-	private int currentDirCount;	// 当前方向的累加
-	private Node currentNode;		// 当前节点
-	private Node startNode;			// 开始节点
-	private Node endNode;			// 结束节点
+	public int currentDirCount;	// 当前方向的累加
+	public Node currentNode;		// 当前节点
+	public Node startNode;			// 开始节点
+	public Node endNode;			// 结束节点
+	public int stepCount;			
 
 	private bool alwaysBacktrace = false;// 是否每走一步, 都尝试回溯.
 
 //	this.trace = [];
 
-	ArrayList trace = new ArrayList();
-	Dictionary<string,Object> neighbors; // 邻居节点
+	public List<Node> trace = new List<Node>();
+	public Dictionary<string,object> neighbors; // 邻居节点
 	public int size;
 
 
@@ -66,9 +69,9 @@ public class MazeBase  : MonoBehaviour {
 
 	// 获取随机数
 	public int random(int min,int max){
-		//		return ((max - min + 1) * Math.random() + min) >> 0; // js
 		System.Random random = new System.Random();
 		return random.Next(min,max)>> 0;
+		//		return ((max - min + 1) * Math.random() + min) >> 0; // js
 	}
 
 	// unused 获取节点
@@ -77,7 +80,8 @@ public class MazeBase  : MonoBehaviour {
 	}
 
 	// 获取随机节点
-	private Node getRandomNode(){
+	public Node getRandomNode(){
+//		int r = this.random (0,(this.height-1));
 		int r = this.random (0,this.height-1);
 		int c = this.random (0,this.width-1);
 		return (Node)this.grid [r] [c];
@@ -94,7 +98,7 @@ public class MazeBase  : MonoBehaviour {
 	}
 
 	// unused
-	private int isMarked(Node node,int value){
+	private bool isMarked(Node node,int value){
 		return (node.value & value) == value;
 	}
 
@@ -115,10 +119,11 @@ public class MazeBase  : MonoBehaviour {
 //		this.isMarked(node, Direction.S)>0?count++:0;
 //		this.isMarked(node, Direction.W)>0?count++:0;
 
-		if (this.isMarked (node, Direction.N) > 0)
-			count++;
+		if (this.isMarked (node, Direction.N))count++;
+		if (this.isMarked (node, Direction.E))count++;
+		if (this.isMarked (node, Direction.S))count++;
+		if (this.isMarked (node, Direction.W))count++;
 			
-
 		return count;
 	}
 
@@ -128,7 +133,7 @@ public class MazeBase  : MonoBehaviour {
 
 		this.neighbors = this.getValidNeighbors(node);
 
-		if (this.neighbors && node.value == 0) {
+		if (this.neighbors != null && node.value == 0) {
 			this.trace.Add(node);
 			this.onTrace(node);
 		}
@@ -159,7 +164,7 @@ public class MazeBase  : MonoBehaviour {
 	public void generate(){
 		this.beforeGenrate();
 		this.setCurrent(this.startNode);
-		int stepCount = 0;
+		this.stepCount = 0;
 		while (this.nextStep()) {
 			stepCount++;
 			if (this.isOver() == true) {
@@ -173,32 +178,33 @@ public class MazeBase  : MonoBehaviour {
 
 
 	public void beforeGenrate(){} 
-	public void afterGenrate(){} 	
+	public virtual void afterGenrate(){} 	
 
 	// 生成迷宫时的提前终止条件
-	public void isOver(){} 	
+	public virtual bool isOver(){return false;} 	
 
 	// 下一步
-	public void nextStep(){
+	public bool nextStep(){
 		//		if (!this.neighbors) {
 		// 没有邻居节点时
 		if (this.neighbors==null||this.neighbors.Count==0) {
 			this.beforeBacktrace();
 			return this.backtrace();
 		}
-		var n = this.getNeighbor();
-		this.moveTo(n[0], n[1]);
+		List<object> n = this.getNeighbor();
+		//		this.moveTo((Node)n[n.Keys.First ()], (int)n["dir"]);
+		this.moveTo((Node)n[0], (int)n[1]);
 		this.updateCurrent();
 		return true;
 	} 	
 
-	public void beforeBacktrace(){} 
-	public void backtrace(){
+	public virtual void beforeBacktrace(){} 
+	public bool backtrace(){
 		int len = this.trace.Count;
 		while (len > 0) {
 			int idx = this.getTraceIndex();
 			Node node = (Node)this.trace[idx];
-			Dictionary<string,Object>  nm = this.getValidNeighbors(node);
+			Dictionary<string,object>  nm = this.getValidNeighbors(node);
 			if (nm!=null&&nm.Count>0) {
 				this.currentNode = node;
 				this.neighbors = nm;
@@ -219,31 +225,32 @@ public class MazeBase  : MonoBehaviour {
 
 		for (var r = y; r < ey; r++) {
 			var row = grid[r];
-			if (!row) {
+			if (row==null) {
 				continue;
 			}
 			for (var c = x; c < ex; c++) {
 				Node node = row[c];
-				if (node) {
+				if (node==null) {
 					node.value = Direction.ALL;
 				}
 			}
 		}
 	}
 
+	// unused
 	private void setBlock(int x, int y, int width, int height) {
 		var grid = this.grid;
 		var ex = x + width;
 		var ey = y + height;
 		for (var r = y; r < ey; r++) {
 			var row = grid[r];
-			if (!row) {
+			if (row==null) {
 				continue;
 			}
 			for (var c = x; c < ex; c++) {
 				Node node = row [c];
-				if (node) {
-					node.value = null;
+				if (node==null) {
+					node.value = 0;
 				}
 			}
 		}
@@ -253,9 +260,9 @@ public class MazeBase  : MonoBehaviour {
       通过重写以下几个方法, 可以实现不同的迷宫效果
     **************************************/
 
-	public Dictionary<string,Object> getValidNeighbors(Node node){
-		ArrayList nList = new ArrayList();  
-		Dictionary<string,Object> nMap = new Dictionary<string, Object>();
+	public Dictionary<string,object> getValidNeighbors(Node node){
+		List<List<object>> nList = new List<List<object>>();  
+		Dictionary<string, object> nMap = new Dictionary<string, object>();
 
 		int c = node.x;
 		int r = node.y;
@@ -265,67 +272,119 @@ public class MazeBase  : MonoBehaviour {
 		dir = Direction.N;
 		nearNode = r > 0 ? this.grid[r - 1][c] : null;
 //		this.isValid(nearNode, node, dir) && nList.Add(nMap.Add(dir,new NearNode(nearNode,dir)));
-		if(this.isValid(nearNode, node, dir))
-			nList.Add(nMap.Add(dir,new NearNode(nearNode,dir)));
+		if (this.isValid (nearNode, node, dir)) {
+			nMap.Add (dir.ToString (), new NearNode (nearNode, dir));
+
+//			nList.Add (nMap);
+
+
+			List<object> list = new List<object>();  
+			list.Add (nearNode);
+			list.Add (dir);
+			nList.Add (list);
+		}
 
 		dir = Direction.E;
-		nearNode = this.grid[r][c + 1];
+		nearNode = c < this.width - 1 ? this.grid [r] [c + 1] : null;
 //		this.isValid(nearNode, node, dir) && nList.Add(nMap.Add(dir,new NearNode(nearNode,dir)));		
-		if(this.isValid(nearNode, node, dir))
-			nList.Add(nMap.Add(dir,new NearNode(nearNode,dir)));
+		if (this.isValid (nearNode, node, dir)) {
+			nMap.Add (dir.ToString (), new NearNode (nearNode, dir));
+			//			nList.Add (nMap);
+			List<object> list = new List<object>();  
+			list.Add (nearNode);
+			list.Add (dir);
+			nList.Add (list);
+		}
 
 		dir = Direction.S;
 		nearNode = r < this.height - 1 ? this.grid[r + 1][c] : null;
 //		this.isValid(nearNode, node, dir) && nList.Add(nMap.Add(dir,new NearNode(nearNode,dir)));
-		if(this.isValid(nearNode, node, dir))
-			nList.Add(nMap.Add(dir,new NearNode(nearNode,dir)));
+		if (this.isValid (nearNode, node, dir)) {
+			nMap.Add (dir.ToString (), new NearNode (nearNode, dir));
+			//			nList.Add (nMap);
+
+
+			List<object> list = new List<object>();  
+			list.Add (nearNode);
+			list.Add (dir);
+			nList.Add (list);
+		}
 
 		dir = Direction.W;
-		nearNode = this.grid[r][c - 1];
+		nearNode = c > 0 ? this.grid [r] [c - 1] : null;
 //		this.isValid(nearNode, node, dir) && nList.Add(nMap.Add(dir,new NearNode(nearNode,dir)));
-		if(this.isValid(nearNode, node, dir))
-			nList.Add(nMap.Add(dir,new NearNode(nearNode,dir)));
+		if (this.isValid (nearNode, node, dir)) {
+			nMap.Add (dir.ToString (), new NearNode (nearNode, dir));
+			//			nList.Add (nMap);
+
+
+			List<object> list = new List<object>();  
+			list.Add (nearNode);
+			list.Add (dir);
+			nList.Add (list);
+		}
 
 		this.updateValidNeighbors(nList, nMap);
 
 		if (nList.Count > 0) {
-			nMap.Add ("list",nList);
+			nMap.Add ("list", nList);
 			return nMap;
 		}
+
+
 		return null;
 	}
 
 	// 更新附近的邻居节点
-	public void updateValidNeighbors(ArrayList nList,Dictionary<int,NearNode> nMap) {
+	public void updateValidNeighbors(List<List<object>> nList,Dictionary<string,object> nMap) {
 		
 	}
 
 	// 验证 附近的节点是否有效
-	public bool isValid(Node nearNode,Node node,int dir) {
+	public virtual bool isValid(Node nearNode,Node node,int dir) {
 		return nearNode != null && nearNode.value == 0;
 	}
 
-	public void updateCurrent() {
+	public virtual void updateCurrent() {
 		if (this.alwaysBacktrace) {
 			this.backtrace();
 		}
 	}
 
 	// 获取邻居
-	public int getNeighbor(){
-		ArrayList list = (ArrayList)this.neighbors["list"];
+	public virtual List<object> getNeighbor(){
+		List<List<object>> list = (List<List<object>>)this.neighbors["list"];
 		// js 中 随机数 0~1
 //		var n = list[list.length * Math.random() >> 0];
-		System.Random rd=new System.Random();
-		double dResult; 
-		dResult=rd.NextDouble();
-		var n = list[list.Count * dResult >> 0];
+
+		System.Random random = new System.Random();
+		int key = random.Next(0,list.Count);
+
+		List<object> n = list[key];
 		return n;
 	}
 	// 根据的节点
-	public int getTraceIndex() {
+	public virtual int getTraceIndex() {
 		int idx = this.trace.Count - 1;
 		return idx;
+	}
+
+	public static class Direction{
+		public static int N = 1;
+		public static int S = 2;
+		public static int E = 4;
+		public static int W = 8;
+		public static int ALL = 1 | 2 | 4 | 8;
+
+		// 对面 对立
+		public static Dictionary<int,int> opposite = new Dictionary<int, int>{{1,2},{2,1},{4,8},{8,4}};
+		// 步骤
+		public static Dictionary<int,int> stepX = new Dictionary<int, int>{{1,0},{2,0},{4,1},{8,-1}};
+		public static Dictionary<int,int> stepY = new Dictionary<int, int>{{1,-1},{2,1},{4,0},{8,0}};
+
+
+	
+
 	}
 
 }
@@ -345,7 +404,7 @@ public class Node{
 
 public class NearNode{
 	public Node node;
-	int dir;
+	public int dir;
 
 	public NearNode(){
 	}
@@ -355,20 +414,3 @@ public class NearNode{
 	}
 }
 
-public static class Direction{
-	public static int N = 1;
-	public static  int S = 2;
-	public static int E = 4;
-	public static int W = 8;
-	public static int ALL = 1 | 2 | 4 | 8;
-
-	// 对面 对立
-	public static Dictionary<int,int> opposite = new Dictionary<int, int>{{1,2},{2,1},{4,8},{8,4}};
-	// 步骤
-	public static Dictionary<int,int> stepX = new Dictionary<int, int>{{1,0},{2,0},{4,1},{8,-1}};
-	public static Dictionary<int,int> stepY = new Dictionary<int, int>{{1,-1},{2,1},{4,0},{8,0}};
-
-
-
-
-}
